7ba4860ecfe1ff30ef294ba41b0544ae4a9d2bdf
Illia Sorokoumov <illia.sorokoumov@gmail.com>
2022-10-07 11:03:19 +0200

    Update weather station code

--- weather-station/weather_station.py
+++ weather-station/weather_station.py
@@ -16,8 +16,13 @@ start_http_server(8000)
 
 try:
     while True:
-        gauge_temperature.set(round(float(sense.get_temperature()), 2))
-        gauge_humidity.set(round(float(sense.get_humidity()), 2))
+        temp_from_humidity = float(sense.get_temperature_from_humidity())
+        temp_from_pressure = float(sense.get_temperature_from_pressure())
+        temp_correction_factor = (temp_from_humidity - temp_from_pressure) / temp_from_pressure
+        humidity = float(sense.get_humidity())
+        corrected_humidity = humidity * (1 - temp_correction_factor)
+        gauge_temperature.set(round(temp_from_pressure, 2))
+        gauge_humidity.set(round(corrected_humidity, 2))
         gauge_pressure.set(round(float(sense.get_pressure()), 2))
         time.sleep(5)
 except Exception as e:
