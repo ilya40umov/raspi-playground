f630b219faa897779e56c4f7ebef9d129209285c
Illia Sorokoumov <illia.sorokoumov@gmail.com>
2022-11-20 11:31:26 +0100

    Add first gpio experiment

---
+++ small-experiments/gpio-button-and-4-leds.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def main():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.OUT)
+    GPIO.setup(3, GPIO.OUT)
+    GPIO.setup(4, GPIO.OUT)
+    GPIO.setup(5, GPIO.OUT)
+    GPIO.setup(21, GPIO.IN)
+
+    while True:
+        if GPIO.input(21) == 0:
+            for pin in range(2, 6):
+                GPIO.output(pin, GPIO.HIGH)
+                time.sleep(0.25)
+                GPIO.output(pin, GPIO.LOW)
+                time.sleep(0.25)
+        time.sleep(0.001)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    finally:
+        GPIO.cleanup()
