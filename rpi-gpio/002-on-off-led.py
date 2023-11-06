59b7f9ed42ddf00774eadd95f68e7e6240a76c14
Illia Sorokoumov <illia.sorokoumov@gmail.com>
2023-11-06 23:49:37 +0100

    Minor improvements

--- rpi-gpio/002-on-off-led.py
+++ rpi-gpio/002-on-off-led.py
@@ -28,5 +28,8 @@ def main():
 if __name__ == "__main__":
     try:
         main()
+    except KeyboardInterrupt:
+        print("Program interrupted.")
     finally:
+        print("Running GPIO cleanup.")
         GPIO.cleanup()
